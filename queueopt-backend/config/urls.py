e01445064ecdb8c2a4f9083e8418367e1feb48b5
sammy-sasa <151463535+sammysamuti@users.noreply.github.com>
2025-12-08 22:47:42 +0300

    Add health check endpoint that returns success response

---
+++ queueopt-backend/config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import health_check
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("simulation.urls")),
+    path("", health_check, name="health_check"),
+]
